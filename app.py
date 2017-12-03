f7b6ccfa247f89c6240faaaf91e9d525cdd6fb24
y4smeen <y4smeen@gmail.com>
2017-12-02 20:21:26 -0500

    Change port

--- app.py
+++ app.py
@@ -174,6 +174,8 @@ def generate_trivia():
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run('0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.debug = True
+    # app.run('0.0.0.0', port=5000)
     # generate_trivia()
